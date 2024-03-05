b1a7be68abf445fad8ae8d8e168dd356213c190e
x-Wawa <66263633+x-Wawa@users.noreply.github.com>
2024-03-05 15:54:56 +0100

    Initial commit

---
+++ doboy.c
@@ -0,0 +1,6 @@
+#include "doboy.h"
+
+int	ft_doboy(int ac, char *av[], char *ev[])
+{
+	return (0);
+}
---
+++ doboy.h
@@ -0,0 +1,4 @@
+#ifndef DOBOY_H
+#define DOBOY_H
+int	ft_doboy(int ac, char *av[], char *ev[]);
+#endif
