755da50efa85036875c3b1f307eb42c24eb4c163
Wawa <66263633+wneel@users.noreply.github.com>
2024-03-05 19:38:23 +0100

    feat: see if inside quote or not

--- wawa.c
+++ wawa.c
@@ -11,8 +11,36 @@
 /* ************************************************************************** */
 
 #include "wawa.h"
+#include <stdio.h>
 
 int	ft_wawa(int ac, char *av[], char *ev[])
 {
+  char *text = "echo '\"Salut les mecs\"'";
+  
+	int i = 0;
+	int in_dquotes = 0;
+  int in_squotes = 0;
+  while (text[i] != '\0')
+  {
+    if (text[i] == '\'')
+    {
+      if (!in_dquotes)
+      {
+        in_squotes = in_squotes == 1 ? 0 : 1;
+      }
+    }
+    if (text[i] == '"')
+    {
+      if (!in_squotes)
+      {
+        in_dquotes = in_dquotes == 1 ? 0 : 1;
+      }
+    }
+    if ((in_dquotes && text[i] != '"') || (in_squotes && text[i] != '\''))
+    {
+      printf("%c", text[i]);
+    }
+    i++;
+  }
 	return (0);
 }
