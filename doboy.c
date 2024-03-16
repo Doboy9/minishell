5485683c7dcdfc517a4b9d2e9ebc00c23c06ed40
Doboy <dorian.boire98@gmail.com>
2024-03-16 16:49:59 +0100

    Feat : Command not found added

--- doboy.c
+++ doboy.c
@@ -6,7 +6,7 @@
 /*   By: dboire <dboire@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:13:22 by dboire            #+#    #+#             */
-/*   Updated: 2024/03/16 16:41:08 by dboire           ###   ########.fr       */
+/*   Updated: 2024/03/16 16:49:44 by dboire           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,24 @@ int	ft_doboy(int ac, char *av[], char *ev[])
 	t_envexp *envexp;
 	
 	envexp = ft_calloc(sizeof(t_envexp), 1);
-	prompt = "easyshell XD >";
+	prompt = "easyshell -_- >";
 	lineread = readline(prompt);
 	while (lineread)
 	{
 		if (ft_strncmp(lineread, "env", 3) == 0)
 			ft_copyenv(ev, envexp);
-		if (ft_strncmp(lineread, "export", 6) == 0)
+		else if (ft_strncmp(lineread, "export", 6) == 0)
 			ft_copyexp(ev, envexp);
-		if (ft_strncmp(lineread, "pwd", 3) == 0)
+		else if (ft_strncmp(lineread, "pwd", 3) == 0)
 			printf("%s\n", getcwd(NULL, 0)); //ok, works
-		if (ft_strncmp(lineread, "cd", 2) == 0) // Besoin de split le line read pour avoir le path
+		else if (ft_strncmp(lineread, "cd", 2) == 0) // Besoin de split le line read pour avoir le path
 			chdir("/nfs/homes"); //ok, working but need modif
-		if (ft_strncmp(lineread, "echo", 4) == 0) // Besoin de split le lineread pour avoir ce qu il faut ecrire
+		else if (ft_strncmp(lineread, "echo", 4) == 0) // Besoin de split le lineread pour avoir ce qu il faut ecrire
 			printf("%s\n", lineread); // ok, working but need modif
-		if (ft_strncmp(lineread, "exit", 4) == 0) // Function to free everything needed
+		else if (ft_strncmp(lineread, "exit", 4) == 0) // Function to free everything needed
 			exit(0); //ok, working but need modif
+		else
+			printf("%s: command not found\n", lineread); //ok, works
 		add_history(lineread);
 		free(lineread);
 		lineread = readline(prompt);
